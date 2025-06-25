4a73bcde14ee59bd426a642f7d6faf529fd38b8d
Dileepa Dilshan <wickramasinghemgdd@gmail.com>
2025-06-25 16:20:34 +0530

    Update firebase.js

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth'; // Import getAuth for Authentication
 // Your web app's Firebase configuration
 // (You'll find this in your Firebase project settings in the console)
 const firebaseConfig = {
-  apiKey: "AIzaSyAupC0d8k3ganIb7KH14U1vehnP9QlydJM",
-  authDomain: "clara-recruiter-rep.firebaseapp.com",
-  projectId: "clara-recruiter-rep",
-  storageBucket: "clara-recruiter-rep.firebasestorage.app",
-  messagingSenderId: "283187403534",
-  appId: "1:283187403534:web:659cd45e75a5fb0540955f",
-  measurementId: "G-WETVFKV4P5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
